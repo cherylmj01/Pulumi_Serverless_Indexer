0252386c232b35639739c3655cc91e02055dd2cb
cherylmj01 <cherylmj01@gmail.com>
2024-03-03 21:54:24 -0700

    Updated the table name

--- lambda_function/process_s3_uploads.py
+++ lambda_function/process_s3_uploads.py
@@ -2,24 +2,14 @@ import json
 import boto3
 from datetime import datetime
 
-# Initialize a Boto3 client for DynamoDB
 dynamodb = boto3.resource('dynamodb')
-
-# Specify DynamoDB table name
-table_name = 'index_table'
+table_name = 'my-serverless-infra-index-table-a24e71b'
 
 def lambda_handler(event, context):
-    # Get the DynamoDB table resource
     table = dynamodb.Table(table_name)
-
     for record in event['Records']:
-        # Get the S3 object key
         s3_object_key = record['s3']['object']['key']
-
-        #Get the current timestamp
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        # Write the object key and timestamp to the DynamoDB table
         try:
             response = table.put_item(
                 Item = {
@@ -30,7 +20,6 @@ def lambda_handler(event, context):
             print(f"DynamoDB Response: {json.dumps(response)}")
         except Exception as e:
             print(f"Error writing to DynamoDB: {str(e)}")
-
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
